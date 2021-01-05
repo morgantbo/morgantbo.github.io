15758baee962a6cb33e47b5236d808f344d208b1
Morgan <mobo@umich.edu>
2021-01-05 18:01:11 -0500

    more changes

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 var scroll = new SmoothScroll('a[href*="#"]',{
     speed: 800
 });
+const navSlide =() =>{
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".w3-right-align");
+    const navLinks = document.querySelectorAll(".w3-bar ul li");
+   
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("w3-bar-active");
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+
+}
+
+navSlide();
