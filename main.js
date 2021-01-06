86b648c3e764c45d784b5c5662760e2ffb801161
Morgan <mobo@umich.edu>
2021-01-06 16:47:38 -0500

    Restructured the Hamburger Menu to reanimate after clicking items inside

--- main.js
+++ main.js
@@ -12,15 +12,32 @@ const navSlide =() =>{
         //Animate Links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = ""
+                link.style.animation = "";
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
+        });
+        navLinks.forEach((item) => {
+            item.addEventListener('click',()=> {
+                if (item.style.animation){
+                    nav.classList.toggle("w3-bar-active");
+                    burger.classList.toggle("toggle");
+                    navLinks.forEach((link,index) => {
+                        if (link.style.animation) {
+                            link.style.animation = "";
+                        }
+                        else {
+                            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                        }
+                    })
+                }
+                
+            });
         });
         //Burger Animation
         burger.classList.toggle("toggle");
     });
 
 }
-
 navSlide();
+
